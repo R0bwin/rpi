ef5adf82fc755aadf6dd031e478bce9a3f65cd79
r0bwin <robin_winroth@hotmail.com>
2020-08-30 16:15:34 +0200

    Update program.js

--- program.js
+++ program.js
@@ -2,8 +2,10 @@ const { Gpio } = require( 'onoff' );
 
 // set BCM 4 pin as 'output'
 const led = new Gpio( '21', 'out' );
-const button = new Gpio( '20', 'in', 'both' );
+// const button = new Gpio( '20', 'in', 'both' );
 
-button.watch((err, value) => led.writeSync(value));
+led.writeSync(1);
+
+//button.watch((err, value) => led.writeSync(value));
 
 console.log("test");
